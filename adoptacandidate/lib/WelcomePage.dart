e5f9f47097b4e14d394bdb2a6dafc9eb856e2587
Mathis LEBEL <145991354+mathislebel@users.noreply.github.com>
2024-05-16 17:45:19 +0200

    initialize the first page

---
+++ adoptacandidate/lib/WelcomePage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:adoptacandidate/widgets/DelayAnimation.dart';
+import 'package:adoptacandidate/widgets/Color.dart';
+
+class WelcomPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF0D2A57),
+    );
+  }
+}
