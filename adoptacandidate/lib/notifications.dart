f178a245b2642bb42ccee2f98112c79c63cf487d
PaulNowak36 <91249965+PaulNowak36@users.noreply.github.com>
2024-06-13 10:16:21 +0200

    Scrollable notifications cards

--- adoptacandidate/lib/notifications.dart
+++ adoptacandidate/lib/notifications.dart
@@ -25,14 +25,13 @@ class _NotificationsPageState extends State<NotificationsPage> {
             ),
           ),
         ),
-        // Optionally, you can add other widgets like actions, leading, etc.
       ),
       backgroundColor: const Color(0xFF0D1B2A),
       body: Container(
-        margin: const EdgeInsets.fromLTRB(0, 40, 0, 0),
-        child: const Column(
+        margin: const EdgeInsets.fromLTRB(0, 40, 0, 40),
+        child: Column(
           children: [
-            Row(
+            const Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 Text(
@@ -48,11 +47,74 @@ class _NotificationsPageState extends State<NotificationsPage> {
                   style: TextStyle(color: Colors.white),
                 ),
               ],
-            )
+            ),
+            // Flexible to ensure it takes the remaining space
+            Flexible(
+              child: ListView(
+                children: const [
+                  // Wrapping Column with padding
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            NotificationCard(),
+                            NotificationCard(),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            NotificationCard(),
+                            NotificationCard(),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            NotificationCard(),
+                            NotificationCard(),
+                          ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            NotificationCard(),
+                            NotificationCard(),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
-      
+    );
+  }
+}
+
+class NotificationCard extends StatelessWidget {
+  const NotificationCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(23)),
+      elevation: 5,
+      margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.grey,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        width: 150,
+        height: 200,
+      ),
     );
   }
 }
